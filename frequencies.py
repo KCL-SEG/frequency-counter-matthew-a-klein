79415b211c236796c32133e0bf63e52fd4be7538
Matthew Klein <matthewklein345@gmail.com>
2022-10-02 10:13:31 +0100

    Complete Solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,29 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+def is_string(item):
+    return isinstance(item, str)
+
+def make_string(item):
+    if is_string(item):
+        pass
+    else:
+        item = str(item)
+    return item
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    string_items = []
+
+    for item in items:
+
+         string_item = make_string(items)
+         string_items.add(string_item)
+
+    for string_item in string_items:
+        if string_item in frequencies.keys():
+            pass
+        else:
+            frequencies.add(string_item, string_items.count(string_item))
+
+
     return frequencies
